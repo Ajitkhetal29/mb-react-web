a4d2377753e1d3e6be73944da36e6d8b2e721501
Ajit Khetal <140830993+Ajitkhetal29@users.noreply.github.com>
2025-10-11 12:40:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(cors());
+
 
 connectDb();
 
