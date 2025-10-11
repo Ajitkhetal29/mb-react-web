142db7397923b615d482b5c4ce1597a13ba3ab87
ajitkhetal <ajitkhetalofficial@gmail.com>
2025-10-11 12:15:54 +0530

    tryiong

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
-app.use(corsOptions)
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
