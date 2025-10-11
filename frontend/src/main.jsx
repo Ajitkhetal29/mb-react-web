f02111db103d1bffe6b1001be556f63d38c9b14e
ajitkhetal <ajitkhetalofficial@gmail.com>
2025-10-11 17:11:43 +0530

    hashrouter added in frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AppConetxtProvider from "./context/context.jsx";
-import {HashRouter} from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
-    <BrowserRouter>
+    <AppConetxtProvider>
       <App />
-    </BrowserRouter>
+    </AppConetxtProvider>
   </HashRouter>
 );
